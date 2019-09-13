1a82606020d9e0ea9b8f4064c24b5f5a8f92a1cb
nyklclhst <irfan.aliyafi@outlook.co.id>
2019-09-13 17:00:36 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import mysql.connector
+import requests
+import schedule
+import time
+from datetime import datetime
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="ali",
+    passwd="nyk",
+    database="seculab"
+)
+
+sts = 0
+mycursor = mydb.cursor()
+myc = mydb.cursor()
+mycursor.execute("Select * from asisten")
+myresult = mycursor.fetchall()
+bot_token = '927752450:AAHMdTm4SgCs7cA-xtryArMw4GbHyJuk-1w'
+
+
+def telegram_bot_sendtext():
+    for x in myresult:
+        msg1 = "Hallo "+str(x[2])+". Jadwal jaga anda hari ini ada di shift:\n"
+        # print(send_text)
+        # requests.get(send_text)
+        myc.execute("select * from jadwal where chat_id="+str(x[1]))
+        res = myc.fetchall()
+        for y in res:
+            if y[1] == datetime.today().strftime('%A') and y[4] == sts:
+                if int(y[2]) != 0:
+                    msg = str(y[2])+"\n"
+                    send_text1 = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(x[1]) + '&parse_mode=Markdown&text='+msg
+                    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(x[1]) + '&parse_mode=Markdown&text='+msg1        
+                    print(send_text)
+                    print(send_text1)
+                    requests.get(send_text)
+                    time.sleep(1)
+                    requests.get(send_text1)
+
+
+def check_sts():
+    global sts
+    if sts % 2 == 0:
+        print('minggu 1')
+        print(sts)
+    else:
+        print('minggu 2')
+        print(sts)
+    if sts == 1:
+        sts = 0
+    else:
+        sts+=1
+
+schedule.every().day.at("16:40").do(telegram_bot_sendtext)
+schedule.every().monday.do(check_sts)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
